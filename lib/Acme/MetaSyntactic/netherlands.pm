5f04a1dc0bf7ba92073e9732435bb55b1402ba49
Abigail <abigail@abigail.nl>
2005-10-20 11:54:12 +0200

    New theme: Dutch provinces.

---
+++ lib/Acme/MetaSyntactic/netherlands.pm
@@ -0,0 +1,29 @@
+package Acme::MetaSyntactic::netherlands;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::Netherlands - Dutch Provinces
+
+=head1 DESCRIPTION
+
+Names of the Dutch provinces.
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+groningen friesland drente overijssel gelderland flevoland
+utrecht noord_holland zuid_holland zeeland noord_brabant limburg
