c42442bd7705ad3e1f1f93e3a30167e74a1e3d37
Abigail <abigail@abigail.nl>
2005-10-24 23:55:38 +0200

    The nine muses from Greek mythology.

---
+++ lib/Acme/MetaSyntactic/muses.pm
@@ -0,0 +1,29 @@
+package Acme::MetaSyntactic::muses;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::muses - Greek Muses.
+
+=head1 DESCRIPTION
+
+The nine muses from Greek mythology.
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>,
+L<http://www.eliki.com/portals/fantasy/circle/define.html>.
+
+=cut
+
+__DATA__
+# names
+calliope clio erato euterpe melpomene polyhymnia terpsichore thalia urania
