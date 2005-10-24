86f9812579bd1fe877bd17eec893ad442b398dea
Abigail <abigail@abigail.nl>
2005-10-24 23:55:38 +0200

    Santa's reindeer.

---
+++ lib/Acme/MetaSyntactic/reindeer.pm
@@ -0,0 +1,31 @@
+package Acme::MetaSyntactic::reindeer;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::reindeer - Santa's Reindeer.
+
+=head1 DESCRIPTION
+
+The names of Santa's Reindeer. Note spelling variations exists, and
+the inclusion of Rudolf is doubtful.
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>,
+L<http://www.pubquizhelp.34sp.com/christmas/reindeer.html>
+
+=cut
+
+__DATA__
+# names
+dasher dancer prancer vixen comet
+cupid donner blitzen rudolf
