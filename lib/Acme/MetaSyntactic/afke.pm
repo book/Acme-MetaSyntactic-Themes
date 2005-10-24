864ce394156458b8d03d6f415d81c9b523b0a9da
Abigail <abigail@abigail.nl>
2005-10-24 23:55:38 +0200

    Character names of a famous Dutch book.

---
+++ lib/Acme/MetaSyntactic/afke.pm
@@ -0,0 +1,31 @@
+package Acme::MetaSyntactic::afke;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::afke - Afke's Children.
+
+=head1 DESCRIPTION
+
+I<Afke's tiental> (I<Afkes ten>) is a classic Dutch childrens book,
+describing the life of the ten children of Afke. This package supplies
+the ten names.
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+eeltje watse klaas jetse jouke
+wiepkje sietske boukje wiebe sipke
