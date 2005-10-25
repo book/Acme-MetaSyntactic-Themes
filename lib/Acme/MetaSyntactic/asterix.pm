149cb6cc44b6cbdac2d977811e60fe726a832862
Abigail <abigail@abigail.nl>
2005-10-25 22:33:13 +0200

    Characters from the Asterix series (11 languages)

---
+++ lib/Acme/MetaSyntactic/asterix.pm
@@ -0,0 +1,238 @@
+package Acme::MetaSyntactic::asterix;
+use strict;
+use Acme::MetaSyntactic::Locale;
+our @ISA = qw( Acme::MetaSyntactic::Locale );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::asterix - Asterix the Gaul
+
+=head1 DESCRIPTION
+
+Characters from the famous French cartoon, created by Goscinny and Uderzo.
+
+=head2 Languages
+
+Supported languages: en us fr nl de es gl hu la pt sv
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::Locale>,
+L<http://en.wikipedia.org/wiki/Asterix>
+L<http://openscroll.org/asterix/characters.html>
+
+=cut
+
+#
+# It seems that in the US different names are used than in the UK. For
+# the names I don't have a US name, I copied the US name.
+#
+# The lists aren't complete.
+#
+
+__DATA__
+# default
+en
+# names en
+Asterix
+Obelix
+Dogmatix
+Getafix
+Vitalstatistix
+Impedimenta
+Cacofonix
+Fulliautomatix
+Unhygienix
+Bacteria
+Geriatrix
+
+Julius_Caesar
+Cleopatra
+Brutus
+Postaldistrix
+Picanmix
+Redbeard
+Pegleg
+Boy
+Panacea
+Ekonomikrisis
+Tragicomix
+Tremendelirius
+Drinklikafix
+# names us
+Asterix
+Obelix 
+Dogmatix 
+Magigimmix
+Arthritix Macroeconomix
+Belladonna
+Malacoustix
+Fulliautomatix
+Epidemix
+Bacteria
+Geriatrix
+
+Julius_Caesar
+Cleopatra
+Brutus
+Postaldistrix
+Picanmix
+Redbeard
+Pegleg
+Boy
+Panacea
+Ekonomikrisis
+Tragicomix
+Tremendelirius
+Drinklikafix
+# names fr
+Astérix
+Obélix
+Idéfix
+Panoramix
+Abraracourcix
+Bonemine
+Assurancetourix
+Cétautomatix
+Ordralfabétix
+Iélosubmarine
+Agecanonix
+
+Jules_César
+Cléopâtre
+Brutus
+Pneumatix
+Keskonrix
+Barbe_Rouge
+
+Baba
+Falbala
+Epidemaïs    
+
+
+Zaza
+Aplusbégalix
+Amérix
+Caïus_Bonus
+# names nl
+Asterix
+Obelix
+Idefix
+Panoramix
+Abraracourcix Heroïx
+
+Assurancetourix Kakofonix
+# names de
+Alkoholix
+Asterix
+Astronomix
+Automatix
+Claudius_Bockschus
+Costa_Y_Bravo
+Brutus
+Julius_Cäsar
+Tullius_Destruktivus
+Eleonoradus
+Falbala
+Gelatine
+Grautvornix
+Gutemine
+Homöopatix
+Acidix_Hydrochloridix
+Idefix
+Jellosubmarine
+Kantine
+Lucius_Keuchhustus
+Kleopatra
+Majestix
+Olaf_Maulaf
+Methusalix
+Miraculix
+Moralelastix
+Nixalsverdrus Hotelterminus
+Numerobis
+Obelix
+Orthopädix
+Osolemirnix
+Ozeanix
+Praline
+Processus
+Feistus_Raclettus
+Gaius_Raffcus
+Rohrpostix
+Salamix
+Studicus
+Taubenus
+Technokratus
+Teefax
+Tragicomix
+Troubadix
+Vaseline
+Verleihnix
+Agrippus_Virus
+Vreneli
+Völligbaf
+Zechine
+# names es
+Astérix
+Obélix
+Panorámix
+Asurancetúrix
+Abraracúrcix
+Karabella
+Ordenalfabétix
+Esautomátix
+Edadepiédrix
+Idefix Ideafix
+Julio_César
+Cleopatra
+# names gl
+Astérix
+Obélix
+Panorámix
+Abraracurcix Abrazopartidix
+Karabella
+Asuracenturix Seguroatodoriesguix
+Ideafix Idefix
+# names hu
+Asterix
+Obelix
+Mirnixdirnix
+Magicoturmix
+Hasarengazfix
+Hangianix
+Messesaglix
+Sokadikix
+# names la
+Asterix
+Obelix
+Idefix
+Miraculix
+Maiestix
+Troubadix
+# names pt
+Asterix
+Obelix
+Chatotorix
+Abracurcix
+Idéiafix
+Panoramix
+# names sv
+Asterix
+Obelix
+Idefix
+Miraculix
+Troubadix
+Majestix
+Senilix
+Smidefix
+Crabbofix
+Bonemine
+Fru_Crabbofix
+Julius_Caesar
