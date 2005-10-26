1dcad774a31b9bc9dcd925a0a92f79c74f460cc5
Abigail <abigail@abigail.nl>
2005-10-26 23:26:26 +0200

    Ben and Jerry: Their current set of flavours.

---
+++ lib/Acme/MetaSyntactic/ben_and_jerry.pm
@@ -0,0 +1,80 @@
+package Acme::MetaSyntactic::ben_and_jerry;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::ben_and_jerry - Ben & Jerry Ice Cream Flavours
+
+=head1 DESCRIPTION
+
+Flavours of the I<Ben & Jerry> ice-cream brand.
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 BUGS
+
+The package doesn't list retired flavours. The Ben & Jerry website
+hides the retired flavours in a hideous flash application.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>,
+L<http://www.benjerry.com/>.
+
+=cut
+
+__DATA__
+# names
+Apple_y_Ever_After
+Berry_Berry_Extraordinary
+Black_Raspberry
+Brownie_Batter
+Butter_Pecan
+Cherry_Garcia
+Cherry_Vanilla
+Chocolate
+Chocolate_Chip_Cookie_Dough
+Chocolate_Fudge_Brownie
+Chocolate_Therapy
+Chubby_Hubby
+Chunky_Monkey
+Coconut_Almond_Fudge_Chip
+Coffee
+Coffee_Coffee_BuzzBuzzBuzz
+Coffee_HEATH_Bar_Crunch
+Cookie_Dough
+Dave_Matthews_Band_Magic_Brownies
+Dublin_Mudslide
+Everything_But_The
+Fossil_Fuel
+Fudge_Central
+Half_Baked
+Karamel_Sutra
+In_A_Crunch
+Mango_Lime
+Marsha_Marsha_Marshmallow
+Mint_Chocolate_Chunk
+New_York_Super_Fudge_Chunk
+Oatmeal_Cookie_Chunk
+Peanut_Butter_Cup
+Phish_Food
+Pistachio_Pistachio
+Strawberry
+Strawberry_Cheesecake
+Strawberry_Kiwi
+Sweet_Cream_Cookies
+The_Gobfather
+The_Last_Straw
+Triple_Caramel_Chunk
+Uncanny_Cashew
+Vanilla
+Vanilla_Almond
+Vanilla_Caramel_Fudge
+Vanilla_HEATH_Bar_Crunch
+Wich_Ice_Cream_Cookie_Sandwich
