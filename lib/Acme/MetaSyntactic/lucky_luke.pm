e59dda87a7d5e3218c112ff5f26a54b2a6833830
Abigail <abigail@abigail.nl>
2005-10-27 22:28:55 +0200

    lucky_luke: Characters from Lucky Luke

---
+++ lib/Acme/MetaSyntactic/lucky_luke.pm
@@ -0,0 +1,34 @@
+package Acme::MetaSyntactic::lucky_luke;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::lucky_luke - Characters from I<Lucky Luke>.
+
+=head1 DESCRIPTION
+
+Characters from the comic I<Lucky Luke>, by Morris et. al.
+
+=head1 BUGS
+
+This list isn't complete.
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Lucky_Luke Jolly_Jumper Rantanplan
+Ma_Dalton Joe_Dalton William_Dalton Jack_Dalton Averell_Dalton
+Calamity_Jane Billy_the_Kid Judge_Roy_Bean Jesse_James
