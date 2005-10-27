7a8ea4bb3826034022cfef518077e51857b034b4
Abigail <abigail@abigail.nl>
2005-10-27 22:28:55 +0200

    renault: All the types of Renault cars

---
+++ lib/Acme/MetaSyntactic/renault.pm
@@ -0,0 +1,50 @@
+package Acme::MetaSyntactic::renault;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::renault - Renault cars
+
+=head1 DESCRIPTION
+
+Car types from the French car manufacturer I<Renault>.
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>,
+L<http://www.renault.com>.
+
+=cut
+
+__DATA__
+# names
+Renault_Ax Renault_12CV Renault_20CV Type_A Type_AG Type_AG_1 Type_AH
+Type_AM Type_AI Type_CF Type_DQ Type_ET Type_B Type_C Type_D Type_E
+Type_G Type_H Type_J Type_L Type_M Type_Na Type_Nb Type_S Type_Nc Type_Q
+Type_Ua Type_Ue Type_R Type_T Type_Ub Type_Uc Type_Ud Type_V Type_AS
+Type_X Type_X_1 Type_Y
+Celtaquatre Juvaquatre Nervastella Primaquatre Reinastella
+Viva_Grand_Sport Vivaquatre Vivasix
+Renault_4CV Caravelle Colorale Dauphine Dauphinoise Floride Frégate Ondine
+Renault_3 Renault_4 Renault_5 Renault_6 Renault_7 Renault_8 Renault_9
+Renault_10 Renault_11 Renault_12 Renault_14 Renault_15 Renault_16
+Renault_17 Renault_18 Renault_19 Renault_20 Renault_21 Renault_25
+Renault_30
+Alliance Encore Fuego Medallion Premier Torino
+Avantime Clio Espace Kangoo Laguna Logan Mégane Modus Safrane Scénic
+Twingo Vel_Satis Spider
+A106 A108 A110 A305 A310 A610
+Argos Be_Bop BRV Espace_F1 Ellypse EPURE EVE Etoile_Filante Fiftie Fluence
+Initiale Koléos Ludo Next Pangea Racoon Talisman Velsatis VESTA_2 Wind
+ZO Zoé
+Estafette Fourgonette Kangoo_Express Kerax Magnum Mascott Master Midlum
+Trafic
+CityBus PR_100
