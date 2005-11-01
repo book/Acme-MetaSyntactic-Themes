632c052db55d405183b25b8e7ebe6163c70c9934
Abigail <abigail@abigail.nl>
2005-11-01 02:57:10 +0100

    calvin: characters from Calvin and Hobbes

---
+++ lib/Acme/MetaSyntactic/calvin.pm
@@ -0,0 +1,41 @@
+package Acme::MetaSyntactic::calvin;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::calvin - Characters from Calvin and Hobbes.
+
+=head1 DESCRIPTION
+
+Characters from the famous comic strip, I<Calvin and Hobbes>,
+by I<Bill Watterson>. The comic ran from November 18th, 1985 till
+December 31, 1995, 3150 strips in total.
+
+=head1 QUOTE
+
+    It's a magical world, Hobbes ol' buddy!
+              Let's go exploring!
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>,
+L<http://en.wikipedia.org/wiki/Calvin_and_Hobbes>
+
+=cut
+
+__DATA__
+# names
+Calvin Hobbes
+Mom Dad 
+Susie_Derkins Miss_Wormwood Rosalyn Moe
+Principal_Spittle Uncle_Max Mr_Lockjaw
+Galaxoid Nebular
+Stupendous_Man Spaceman_Spiff Tracer_Bullet Captain_Napalm God
