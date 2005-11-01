0ad86645c313d0c24d0bcbd89ab172c3186b4523
Abigail <abigail@abigail.nl>
2005-11-01 22:04:23 +0100

    The smurfs.

---
+++ lib/Acme/MetaSyntactic/smurfs.pm
@@ -0,0 +1,33 @@
+package Acme::MetaSyntactic::smurfs;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::smurfs - Smurfs.
+
+=head1 DESCRIPTION
+
+Various of the smurfs (and supporting characters).
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Airborne Alchemist Baby Baker Balthazar Bashful Brainy Clockwork
+Clumsy Dabbler Doc Dopey Dreamy Farmer Finance Fisher Gargamel
+Grandpa Greedy Grouchy Handy Harmony Hefty Johan Jokey King Miner
+Nanny Nat Nosey Painter Papa Poet Princess_Salvina Reflection
+Sassette Scaredy Sleepy Sloppy Slouchy Smoogle Smurfette Sneezy
+Sweepy Tailor Timid Tracker Vanity Weakling Wild Wooley
