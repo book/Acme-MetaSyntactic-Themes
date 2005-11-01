1d5082da279aa3afe1f451539cea68578df6ec51
Abigail <abigail@abigail.nl>
2005-11-01 02:57:10 +0100

    tarot: Tarot cards.

---
+++ lib/Acme/MetaSyntactic/tarot.pm
@@ -0,0 +1,41 @@
+package Acme::MetaSyntactic::tarot;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::tarot - Tarot cards
+
+=head1 DESCRIPTION
+
+Tarot decks consist of 78 different cards - a 22 card I<Major Arcana>,
+and 4 14-card suits forming the I<Minor Arcana>. The suits in the
+minor arcana as I<Wands>, I<Cups>, I<Swords> and I<Pentacles>. Ranks
+start with I<Ace>, then go from 2 to 10 inclusive, then I<Page>,
+I<Knight>, I<Queen> and I<King>. In the Major Arcana, we find:
+I<Fool>, I<Magician>, I<High Priestess>, I<Empress>, I<Emperor>,
+I<Hierophant>, I<Lovers>, I<Chariot>, I<Strength>, I<Hermit>,
+I<Wheel of Fortune>, I<Justice>, I<Hanged Man>, I<Death>, I<Temperance>,
+I<Devil>, I<Tower>, I<Star>, I<Moon>, I<Sun>, I<Judgement>, and finally,
+I<World>.
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>,
+L<http://www.learntarot.com/>
+
+=cut
+
+__DATA__
+# names
+Fool Magician High_Priestess Empress Emperor Hierophant Lovers
+Chariot Strength Hermit Wheel_of_Fortune Justice Hanged_Man Death
+Temperance Devil Tower Star Moon Sun Judgement World
+Wand Cup Sword Pentacle Page Knight Queen King Ace
