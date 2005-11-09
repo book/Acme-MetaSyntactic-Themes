56b94f327df7514f33ccc4a0bf274c761fdfcaef
Abigail <abigail@abigail.nl>
2005-11-09 17:52:09 +0100

    Just a small theme this time: the Beatles.

---
+++ lib/Acme/MetaSyntactic/beatles.pm
@@ -0,0 +1,28 @@
+package Acme::MetaSyntactic::beatles;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::beatles - The Fab 4!
+
+=head1 DESCRIPTION
+
+The names of one of the most well-known and best pop groups ever.
+
+=head1 CONTRIBUTOR
+
+Original contributor: Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+John Paul Ringo George
