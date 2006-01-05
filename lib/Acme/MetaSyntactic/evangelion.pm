e66825462d212f94746628aba4ab4929d45179bf
Sébastien Aperghis-Tramoni <saper@cpan.org>
2006-01-05 01:08:17 +0100

    Theme "evangelion" pour AMS

---
+++ lib/Acme/MetaSyntactic/evangelion.pm
@@ -0,0 +1,80 @@
+package Acme::MetaSyntactic::evangelion;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw(Acme::MetaSyntactic::List);
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::evangelion - The Neon genesis Evangelion theme
+
+=head1 DESCRIPTION
+
+This theme provides the English names of the characters from the 
+Japanese animated series I<Neon Genesis Evangelion>, and also other 
+terms used in the series. 
+
+L<http://en.wikipedia.org/wiki/Neon_Genesis_Evangelion> is a good 
+start to read about about I<Evangelion>. 
+
+=head1 CONTRIBUTOR
+
+SE<eacute>bastien Aperghis-Tramoni.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# Children / pilots
+Ayanami Rei
+Soryu Asuka Langley
+Ikari Shinji
+Suzuhara Toji
+Nagisa Kaworu
+
+# Gehirn / Nerv staff
+Ikari Yui
+Ikari Gendoo
+Fuyutsuki Kozo
+Akagi Ritsuko
+Katsuragi Misato
+Ryouji Kaji
+Ibuki Maya
+Hyuga Makoto
+Shigeru Aoba
+Soryu Zeppelin Kyoko
+Akagi Naoko
+
+# Seele staff
+Lorenz Keel
+
+# MAGI
+MAGI Melchior Balthasar Casper
+
+# Evas
+Zerogoki Shogoki Nigoki Sangoki Yongoki Ryousanki
+
+# Shitos
+Adam Lilith Sachiel Samsiel Ramiel Gagiel Israfel Sandalfon Matarael
+Saraqiel Iroel Leliel Bardiel Zeruel Arael Armisael Tabris Lilim
+
+# other characters
+Aida kensuke
+Horaki Hikari
+Pen_Pen
+
+# glossary
+GEHIRN NERV Marduk SEELE
+Sephiroth
+Eva Evangelion Shito Angel
+B_type D_Type F_type
+entry_plug dummy_plug LCL
+progressive_knife lance_of_Longinus 
+AT_Field S2_Engine
+N2_mine Jet_Alone
+Tokyo_3 GeoFront Central_Dogma Terminal_Dogma
+First_Impact Second_Impact Third_Impact
