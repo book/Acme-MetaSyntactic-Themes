8b7de3ee23f8fcb54ef5735eb03f9c5ac692bdfc
Abigail <abigail@abigail.nl>
2006-05-13 20:33:22 +0200

    Acme::MetaSyntactic::screw

---
+++ lib/Acme/MetaSyntactic/screw.pm
@@ -0,0 +1,28 @@
+package Acme::MetaSyntactic::screw;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::screw - The screw theme
+
+=head1 DESCRIPTION
+
+This theme lists different screw types.
+
+=head1 CONTRIBUTOR
+
+Abigail
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+slotted Phillips PoziDriv Robertson SupaDriv TORX Allen
