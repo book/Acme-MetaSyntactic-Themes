21a376f6f4dafa4366137fd18f3861122c14af95
Laurent GAUTROT <l.gautrot@free.fr>
2006-05-31 20:48:48 +0200

    Acme::Metasyntactic::mars ?

---
+++ lib/Acme/MetaSyntactic/mars.pm
@@ -0,0 +1,34 @@
+From: Laurent GAUTROT <l.gautrot@free.fr>
+To: philippe.bruhat@free.fr
+Subject: Acme::Metasyntactic::mars ?
+Date: Wed, 31 May 2006 20:48:48 +0200
+Message-Id: <200605312048.48514.l.gautrot@free.fr>
+
+Hello,
+
+Je suis replongé dans la trilogie martienne de Kim Stanley Robinson.
+C'est la faute à Stef Payrard. ;o)
+
+Il appararît de manière récurrente une liste des noms de Mars.
+
+Peut-être suffisante pour un Acme::Metasyntactic::mars ?
+
+Al-Qahira
+Arès
+Auqakuh
+Bahram
+Harmakhis
+Hrad
+Huo Hsing
+Kasei
+Ma'adim
+Maja
+Mamers
+Mangala
+Mars
+Mawrth
+Nirgal
+Shalbatanu
+Simud
+Tiu
+
