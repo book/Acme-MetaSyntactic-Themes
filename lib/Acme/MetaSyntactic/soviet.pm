2e4c17d69752d6908176740b03c1b44320aae16b
Jean Forget <ponder.stibbons@free.fr>
2006-07-05 07:58:35 +0200

    Acme::MetaSyntactic::soviet
    Est-ce qu'on a le droit d'utiliser le POD
    de ACME::MS comme une tribune libre ?
    cf le nouveau module soviet.pm
    Je t'envoie également la version annotée
    qui te montrera que le classement des
    aéronefs et des missiles n'est pas au hasard.
    Tu as également les numéros de pages des radars dans
    Flottes de Combat et dans le Pat-Led, mais
    cela ne t'avancera pas à grand-chose si
    tu n'as pas toi-même ces livres.
    
    Et à propos des sources, remarque que certaines
    sont "written" et les autres "designed".

---
+++ lib/Acme/MetaSyntactic/soviet.pm
@@ -0,0 +1,110 @@
+package Acme::MetaSyntactic::soviet;
+use strict;
+use Acme::MetaSyntactic::MultiList;
+our @ISA = qw( Acme::MetaSyntactic::MultiList );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::soviet -- NATO codenames for Soviet-designed equipment
+
+=head1 DESCRIPTION
+
+Some codenames given by NATO to Soviet-designed aircraft, missiles and radars
+
+=head1 CONTRIBUTOR
+
+Jean Forget
+
+Introduced in version 0.xx, published on xx, 2006.
+
+=head1 SOURCES
+
+I<Air Superiority>
+designed by J.D. Webster
+and published by GDW
+(ISBN 0-943580-19-6).
+
+I<Air Strike>
+designed by J.D. Webster
+and published by GDW
+(ISBN 0-943580-30-7).
+
+I<Desert Falcons>
+designed by J.D. Webster
+and published by GDW
+(ISBN 0-943580-97-8).
+
+I<Flottes de Combat 1990>
+written by B. Prézelin
+published by Editions Maritimes et Outre-Mer
+(ISBN 2.7373.0485.7).
+
+I<Pat-Led-1>
+written by J-J Patry and P. Lederer
+published by Editions Presse & Recherche.
+
+I<Jane's World Aircraft Recognition Handbook>
+written by Derek Wood
+published by Jane's
+(ISBN 0-7106-0343-6).
+
+=head1 IMPORTANT NOTE
+
+The contributor would like to give the following warning and rant.
+
+I have used only sources published in 1990 or earlier. I have done
+so to avoid the use of the "ex-" prefix. I am fed up with the silly
+practice which appeared in 1991 or so, consisting in prefixing
+everything Soviet and Jugoslavian with "ex-". You could even find
+sentences such as: "On June 22nd, 1941, the Third Reich attacked
+the ex-USSR. On the first week, several hundred thousand soldiers
+from the ex-Red Army were taken prisoners by the Wehrmacht."
+This is particularly stupid since the Red Army and the USSR outlasted
+the Third Reich and the Wehrmacht by 45 years.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::MultiList>.
+
+=cut
+
+__DATA__
+# default
+radars
+# names radars
+Ball_End Band_Stand Bar_Lock Bee_Hind Big_Fred Big_Net Big_Screen
+Blue_Jay Brass_Tilt Cake_Stand Cross_Bird Cross_Sword Dog_Ear
+Don_Kay Down_Beat Drum_Tilt Egg_Cup Eye_Bowl Fire_Dome Flap_Lid
+Flat_Face Foxfire Front_Door Front_Piece Gun_Dish Half_Cup
+Hawk_Screech Head_Lights Head_Net High_Fix High_Lark High_Sieve
+Hot_Shot Jaybird Kite_Screech Land_Roll Long_Track Muff_Cob
+Owl_Screech Palm_Frond Pat_Hand Peel_Cone Peel_Group Plank_Shave
+Plate_Steer Plinth_Net Pop_Group Pork_Through Puff_Ball Scoop_Pair
+Side_Net Skip_Spin Sky_Watch Slim_Net Small_Fred Small_Yawn Snoop_Pair
+Snoop_Plate Snoop_Slab Snoop_Tray Spin_Trough Spoon_Rest Square_Tie
+Squeeze_Box Straight_Flush Strut_Curve Strut_Pair Sun_Visor
+Tall_Mike Tee_Plinth Thin_Skin Tin_Man Top_Dome Top_Knot Top_Pair
+Top_Plate Top_Sail Top_Steer Top_Trough Trap_Door Two_Spots
+# names aircraft
+Badger Bear Blinder Backfire Bear Beagle Bison Brewer
+Colt Cub Clod Cock Coke Curl Cash Clank Cline Condor
+Crate Coot Classic Candid Camber
+Crusty Careless
+Codling Clobber
+Fagot Fresco Farmer Fishbed Flogger Foxbat Fulcrum Foxhound
+Fitter Fishpot Flagon Fencer Frogfoot Flanker Fiddler
+Firebar Forger
+Hare Hoplite Hound Hook Hip Harke Haze Hind Halo Havoc
+Hormone Hoodlum Helix Hokum
+Mail Mascot May Midget Moss Max Moose Mangrove
+# names missiles
+Atoll Anab Ash Acrid Apex Aphid Amos Alamo Archer
+Guideline Goa Ganef Gainful Goblet Gecko Gaskin Grail Grumble Gadfly
+Gladiator Gopher Gremlin
+Kennel Kipper Kangaroo Kitchen Kelt Kingfish Kerry Kyle Karen Kedge
+Kent
+Styx Shaddock Sark Serb Starbright Sawfly Siren Sandbox Silex Starfish
+Stallion Snipe Stingray Shipwreck Sturgeon Sampson Sunburn Skiff
+Scorpion Swatter Sagger Spigot Spandrel Spiral Scarab Spider Scud
