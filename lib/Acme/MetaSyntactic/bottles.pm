a8f1f30587a063b4733dce1caaa729937c252bde
Jean Forget <ponder.stibbons@wanadoo.fr>
2006-08-09 07:48:30 +0200

    un autre module bottles.pm

---
+++ lib/Acme/MetaSyntactic/bottles.pm
@@ -0,0 +1,53 @@
+package Acme::MetaSyntactic::bottles;
+use strict;
+use Acme::MetaSyntactic::MultiList;
+our @ISA = qw( Acme::MetaSyntactic::MultiList );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::bottles - The kings and patriarchs and private eyes theme.
+
+=head1 DESCRIPTION
+
+This list is a mixed bag containing ancient kings,
+biblical patriarchs and an Hawaiian private eye.
+
+Yet, some people pretend this list is a companion
+to F<Acme::MetaSyntactic::booze>.
+
+Sources: L<http://fr.wikipedia.org/wiki/Vin_de_Champagne#Flacons>
+and L<http://www.diracdelta.co.uk/science/source/b/o/bottle/source.html>.
+
+=head1 CONTRIBUTOR
+
+Jean Forget.
+
+Introduced in version 0.xx, published on xxx, 2006.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::MultiList>.
+
+=cut
+
+__DATA__
+# names fr
+bouteille
+magnum
+jeroboam
+rehoboam
+mathusalem
+salmanazar
+balthazar
+nabuchodonosor
+# names en
+bottle
+magnum
+jeroboam
+rehoboam
+methusalah
+salmanazar
+balthazar
+nebuchadnezzar
