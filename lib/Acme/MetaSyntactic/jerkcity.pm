4a0c5d561f04ae6afb1437ba68ae864d4e8e1e6a
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    jerkcity update for v1.000

--- lib/Acme/MetaSyntactic/jerkcity.pm
+++ lib/Acme/MetaSyntactic/jerkcity.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::jerkcity;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -20,7 +21,10 @@ See L<http://www.jerkcity.com/> for details.
 
 Rafael Garcia-Suarez.
 
-Introduced in version 0.37, published on August 29, 2005.
+Introduced in Acme-MetaSyntactic version 0.37, published on August 29, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
