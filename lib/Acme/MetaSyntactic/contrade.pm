eabec06f2e48152401a517b2e3fb80958bba693d
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    contrade update for v1.000

--- lib/Acme/MetaSyntactic/contrade.pm
+++ lib/Acme/MetaSyntactic/contrade.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::contrade;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -18,7 +19,10 @@ probably the 17 that race in the Palio di Siena.
 
 Estelle Souche
 
-Introduced in version 0.65, published on March 13, 2006.
+Introduced in Acme-MetaSyntactic version 0.65, published on March 13, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
