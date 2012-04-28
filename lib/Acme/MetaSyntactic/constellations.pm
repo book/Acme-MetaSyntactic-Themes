dc8196ed4acf98b4cf56130a24542c21ad81ed6d
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:08 +0200

    constellations update for v1.000

--- lib/Acme/MetaSyntactic/constellations.pm
+++ lib/Acme/MetaSyntactic/constellations.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::constellations;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -20,7 +21,10 @@ L<http://en.wikipedia.org/wiki/List_of_stars_by_constellation>.
 
 Philippe "BooK" Bruhat.
 
-Introduced in version 0.44, published on October 17, 2005.
+Introduced in Acme-MetaSyntactic version 0.44, published on October 17, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
