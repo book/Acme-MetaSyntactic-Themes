a72a055bfbfd16be54d330800d7007dd6b64a91f
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:08 +0200

    antlers update for v1.000

--- lib/Acme/MetaSyntactic/antlers.pm
+++ lib/Acme/MetaSyntactic/antlers.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::antlers;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -17,7 +18,10 @@ Kinds of moose and other antlered, er, moose.
 
 Gaal Yahas.
 
-Introduced in version 0.46, published on October 31, 2005.
+Introduced in Acme-MetaSyntactic version 0.46, published on October 31, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
