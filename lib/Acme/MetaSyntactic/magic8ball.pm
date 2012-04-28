9b12de318cbc67b3d935eeffd41124c8109b8c3d
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    magic8ball update for v1.000

--- lib/Acme/MetaSyntactic/magic8ball.pm
+++ lib/Acme/MetaSyntactic/magic8ball.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::magic8ball;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -27,9 +28,12 @@ L<http://8ball.ofb.net/answers.html>.
 
 Philippe "BooK" Bruhat.
 
-Introduced in version 0.19, published on April 25, 2005.
+Introduced in Acme-MetaSyntactic version 0.19, published on April 25, 2005.
 
 Corrected (4 items were missing) in version 0.24, published on May 30, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
