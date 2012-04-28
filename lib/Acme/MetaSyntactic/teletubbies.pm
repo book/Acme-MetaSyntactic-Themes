1c44920baffabca347d170e1bcb1c4a5e33cfb08
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:10 +0200

    teletubbies update for v1.000

--- lib/Acme/MetaSyntactic/teletubbies.pm
+++ lib/Acme/MetaSyntactic/teletubbies.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::teletubbies;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -17,7 +18,10 @@ Over the hills and far away, Teletubbies come to play.
 
 Philippe "BooK" Bruhat.
 
-Introduced in version 0.23, published on May 23, 2005.
+Introduced in Acme-MetaSyntactic version 0.23, published on May 23, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 ACKNOWLEDGMENT
 
