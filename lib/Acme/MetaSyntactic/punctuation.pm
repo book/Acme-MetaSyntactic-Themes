1bec94ddba9e30aeb2b87ef6e3bfac034ded9b8a
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:10 +0200

    punctuation update for v1.000

--- lib/Acme/MetaSyntactic/punctuation.pm
+++ lib/Acme/MetaSyntactic/punctuation.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::punctuation;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -20,9 +21,12 @@ L<http://en.wikipedia.org/wiki/Punctuation>.
 
 Philippe "BooK" Bruhat.
 
-Introduced in version 0.29, published on July 4, 2005.
+Introduced in Acme-MetaSyntactic version 0.29, published on July 4, 2005.
 
 Updated by Abigail in version 0.91, publish on September 11, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
