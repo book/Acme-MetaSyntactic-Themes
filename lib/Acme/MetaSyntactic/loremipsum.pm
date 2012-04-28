6497e19ab592af97cf7b367630a8069da5a5231f
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    loremipsum update for v1.000

--- lib/Acme/MetaSyntactic/loremipsum.pm
+++ lib/Acme/MetaSyntactic/loremipsum.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::loremipsum;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -25,7 +26,10 @@ Confer L<http://www.lipsum.com/>.
 
 Jérôme Fenal.
 
-Introduced in version 0.13, published on March 14, 2005.
+Introduced in Acme-MetaSyntactic version 0.13, published on March 14, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
