8d1447acddc8c01563377f4db45a8642a1629432
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    pasta update for v1.000

--- lib/Acme/MetaSyntactic/pasta.pm
+++ lib/Acme/MetaSyntactic/pasta.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::pasta;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -23,7 +24,10 @@ L<http://www.professionalpasta.it/>.
 
 Philippe "BooK" Bruhat.
 
-Introduced in version 0.25, published on June 6, 2005.
+Introduced in Acme-MetaSyntactic version 0.25, published on June 6, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
