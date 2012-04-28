ac4c7eddcfcef504dbf438f56f26c9273a58cfda
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    discworld update for v1.000

--- lib/Acme/MetaSyntactic/discworld.pm
+++ lib/Acme/MetaSyntactic/discworld.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::discworld;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -17,9 +18,12 @@ This theme contains items from Terry Pratchett's Discworld series.
 
 Martin Vorländer.
 
-Introduced in version 0.42, published on October 3, 2005.
+Introduced in Acme-MetaSyntactic version 0.42, published on October 3, 2005.
 
 Updated by Jean Forget in version 0.47, published on November 7, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
