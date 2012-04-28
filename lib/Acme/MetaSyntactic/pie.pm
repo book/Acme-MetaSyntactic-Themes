22f11dc96bf63397447947ee446f702fcbd92431
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    pie update for v1.000

--- lib/Acme/MetaSyntactic/pie.pm
+++ lib/Acme/MetaSyntactic/pie.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::pie;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -19,7 +20,10 @@ A list of English pies.
 
 Nicholas Clark
 
-Introduced in version 0.86, published on August 7, 2006.
+Introduced in Acme-MetaSyntactic version 0.86, published on August 7, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
