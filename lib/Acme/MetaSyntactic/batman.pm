613032f960b06cb854f96caf08e9514f017d1e43
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:08 +0200

    batman update for v1.000

--- lib/Acme/MetaSyntactic/batman.pm
+++ lib/Acme/MetaSyntactic/batman.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::batman;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -23,7 +24,10 @@ L<http://www.batmania.com.ar/paginas/serie_onomatopeyas.htm>.
 
 Philippe Bruhat.
 
-Introduced in version 0.01, published on January 14, 2005.
+Introduced in Acme-MetaSyntactic version 0.01, published on January 14, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
