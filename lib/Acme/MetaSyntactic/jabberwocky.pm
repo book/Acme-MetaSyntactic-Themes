b3246c0861a3d216b796643e811f85b77c057f53
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    jabberwocky update for v1.000

--- lib/Acme/MetaSyntactic/jabberwocky.pm
+++ lib/Acme/MetaSyntactic/jabberwocky.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::jabberwocky;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 
 =head1 NAME
 
@@ -99,7 +100,10 @@ L<http://perlmonks.org/?node_id=195873> by RMGir.
 
 Abigail
 
-Introduced in version 0.93, published on September 25, 2006.
+Introduced in Acme-MetaSyntactic version 0.93, published on September 25, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
