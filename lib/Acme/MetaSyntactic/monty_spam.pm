db6c4b6c0dfa976679568939c83fafca0988d566
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    monty_spam update for v1.000

--- lib/Acme/MetaSyntactic/monty_spam.pm
+++ lib/Acme/MetaSyntactic/monty_spam.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::monty_spam;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -26,7 +27,10 @@ L<http://www.cusd.claremont.edu/~mrosenbl/spamfiction.html#mp>.
 
 Philippe "BooK" Bruhat, inspired by Ricardo Signes.
 
-Introduced in version 0.31, published on July 18, 2005.
+Introduced in Acme-MetaSyntactic version 0.31, published on July 18, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
