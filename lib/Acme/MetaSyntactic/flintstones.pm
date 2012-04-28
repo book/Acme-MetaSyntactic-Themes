ac9059a9f1da84e4525168206b7c6654f8a9b940
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    flintstones update for v1.000

--- lib/Acme/MetaSyntactic/flintstones.pm
+++ lib/Acme/MetaSyntactic/flintstones.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::flintstones;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -17,7 +18,10 @@ The characters from the popular serial.
 
 Philippe "BooK" Bruhat.
 
-Introduced in version 0.01, published on January 14, 2005.
+Introduced in Acme-MetaSyntactic version 0.01, published on January 14, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
