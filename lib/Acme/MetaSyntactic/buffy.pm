e65a58ea72e14db1fdd8485634ec0be9c642e175
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:08 +0200

    buffy update for v1.000

--- lib/Acme/MetaSyntactic/buffy.pm
+++ lib/Acme/MetaSyntactic/buffy.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::buffy;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -18,7 +19,10 @@ Courtesy of L<http://buffyology.johnhorner.nu/>.
 
 Rafael Garcia-Suarez.
 
-Introduced in version 0.09, published on February 7, 2005.
+Introduced in Acme-MetaSyntactic version 0.09, published on February 7, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
