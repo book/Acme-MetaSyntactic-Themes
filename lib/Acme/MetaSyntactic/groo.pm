97a14cb8ba6af945aaefabe6b2f324ffca4e889f
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    groo update for v1.000

--- lib/Acme/MetaSyntactic/groo.pm
+++ lib/Acme/MetaSyntactic/groo.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::groo;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -22,9 +23,12 @@ L<http://www.sergioaragones.com/>.
 
 Philippe "BooK" Bruhat.
 
-Introduced in version 0.27, published on June 21, 2005.
+Introduced in Acme-MetaSyntactic version 0.27, published on June 21, 2005.
 
 Corrected a typo in version 0.82, published on July 10, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
