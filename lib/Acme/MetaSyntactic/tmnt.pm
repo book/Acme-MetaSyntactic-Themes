e700fd0b0b08faa583f830519fd37bc26f6c8690
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:10 +0200

    tmnt update for v1.000

--- lib/Acme/MetaSyntactic/tmnt.pm
+++ lib/Acme/MetaSyntactic/tmnt.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::tmnt;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 
 our %Remote = (
@@ -34,9 +35,12 @@ the TMNT, see L<http://www.ninjaturtles.com/>.
 
 Abigail
 
-Introduced in version 0.58, published on January 23, 2006.
+Introduced in Acme-MetaSyntactic version 0.58, published on January 23, 2006.
 
 Made updatable in version 0.59, published on January 30, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
