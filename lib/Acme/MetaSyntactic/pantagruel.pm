e53f794f09d4b56bd48acaf284a4cebdb483aa8c
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    pantagruel update for v1.000

--- lib/Acme/MetaSyntactic/pantagruel.pm
+++ lib/Acme/MetaSyntactic/pantagruel.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::pantagruel;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -19,7 +20,10 @@ List taken from I<Pantagruel>, chapter 2, by Maistre Françoys Rabelais.
 
 Rafael Garcia-Suarez
 
-Introduced in version 0.72, published on May 1, 2006.
+Introduced in Acme-MetaSyntactic version 0.72, published on May 1, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
