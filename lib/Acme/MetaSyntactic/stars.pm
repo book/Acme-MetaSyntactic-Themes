c01686202c19bfdfd9f993f111696081cb59983b
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:10 +0200

    stars update for v1.000

--- lib/Acme/MetaSyntactic/stars.pm
+++ lib/Acme/MetaSyntactic/stars.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::stars;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -18,7 +19,10 @@ L<http://en.wikipedia.org/wiki/List_of_traditional_star_names>.
 
 Rafael Garcia-Suarez.
 
-Introduced in version 0.39, published on September 12, 2005.
+Introduced in Acme-MetaSyntactic version 0.39, published on September 12, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
