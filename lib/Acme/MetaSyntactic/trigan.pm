63ac7b3aa72bdd0cec81e7ea3bd278db2bd108a5
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:10 +0200

    trigan update for v1.000

--- lib/Acme/MetaSyntactic/trigan.pm
+++ lib/Acme/MetaSyntactic/trigan.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::trigan;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -21,7 +22,10 @@ Learn all about it on L<http://www.trigan.com/>.
 
 Abigail
 
-Introduced in version 0.64, published on March 6, 2006.
+Introduced in Acme-MetaSyntactic version 0.64, published on March 6, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
