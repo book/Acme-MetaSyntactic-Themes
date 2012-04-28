39d96223f6779f019f2bd2ad3c1d93cc642c0385
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:10 +0200

    shadok update for v1.000

--- lib/Acme/MetaSyntactic/shadok.pm
+++ lib/Acme/MetaSyntactic/shadok.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::shadok;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -17,7 +18,10 @@ The whole shadok vocabulary. 4 words.
 
 Philippe "BooK" Bruhat.
 
-Introduced in version 0.01, published on January 14, 2005.
+Introduced in Acme-MetaSyntactic version 0.01, published on January 14, 2005.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
