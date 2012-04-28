d9e7a2d5722c55a57675e94b5e12b48c6648390e
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:09 +0200

    pooh update for v1.000

--- lib/Acme/MetaSyntactic/pooh.pm
+++ lib/Acme/MetaSyntactic/pooh.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::pooh;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -29,9 +30,12 @@ Original contributor: Abigail (late October 2005)
 Proposed independently a week later (early November 2005) by Leon Brocard,
 with a few additions.
 
-Introduced in version 0.61, published on February 13, 2006.
+Introduced in Acme-MetaSyntactic version 0.61, published on February 13, 2006.
 
 Updated in version 0.64, published on March 6, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
