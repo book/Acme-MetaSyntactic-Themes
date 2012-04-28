e2c5aa00293bcb4ecc93335f0b0c7966d0bbab8d
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:10 +0200

    pop2 update for v1.000

--- lib/Acme/MetaSyntactic/pop2.pm
+++ lib/Acme/MetaSyntactic/pop2.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::pop2;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -24,7 +25,10 @@ This is a much shorter history than POP3's.
 
 Philippe "BooK" Bruhat.
 
-Introduced in version 0.68, published on April 3, 2006.
+Introduced in Acme-MetaSyntactic version 0.68, published on April 3, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
