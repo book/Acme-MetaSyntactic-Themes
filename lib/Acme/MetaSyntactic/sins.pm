5c8967b4b8ec07e136ee4af213459477863087d1
Philippe Bruhat (BooK) <book@cpan.org>
2012-04-28 23:08:10 +0200

    sins update for v1.000

--- lib/Acme/MetaSyntactic/sins.pm
+++ lib/Acme/MetaSyntactic/sins.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::sins;
 use strict;
 use Acme::MetaSyntactic::Locale;
 our @ISA = qw( Acme::MetaSyntactic::Locale );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -38,7 +39,10 @@ French: L<http://mapage.noos.fr/lesaviezvous/cps/seven.htm>.
 
 Abigail
 
-Introduced in version 0.87, published on August 14, 2006.
+Introduced in Acme-MetaSyntactic version 0.87, published on August 14, 2006.
+
+Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
