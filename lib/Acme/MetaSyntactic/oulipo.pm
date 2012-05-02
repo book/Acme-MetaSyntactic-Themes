2da75052a6af36e6c947ffdc4002cb52ea584527
Philippe Bruhat (BooK) <book@cpan.org>
2012-05-03 01:21:22 +0200

    oulipo update for v1.000

--- lib/Acme/MetaSyntactic/oulipo.pm
+++ lib/Acme/MetaSyntactic/oulipo.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::oulipo;
 use strict;
 use Acme::MetaSyntactic::List;
 our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -23,7 +24,11 @@ See the official Oulipo web site at L<http://www.oulipo.net/>.
 Philippe "BooK" Bruhat (co-creator of the first Oulipo web site, back
 in 1995).
 
-Introduced in version 0.28, published on June 27, 2005.
+Introduced in Acme-MetaSyntactic version 0.28, published on June 27, 2005.
+
+Updated with the new Oulipo members since 2007, and
+received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
 
 =head1 SEE ALSO
 
@@ -33,6 +38,7 @@ L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
 
 __DATA__
 # names
-NA  MB  VB  JB  CB  AB  PB  IC  FC  BC  RC  SC
-MD  JD  LE  FF  PF  AG  MG  JJ  AL  FLL HLT JL
-HM  MM  IM  OP  GP  RQ  JQ  PR  JR  OS  AMS
+NA  MA  MB  VB  JB  CB  AB  PB  IC  FC  BC  RC
+SC  MD  JD  LE  FF  PF  AFG MG  JJ  AL  FLL HLT
+JL  DLB HM  MM  IM  OP  GP  RQ  JQ  PR  JR  OS
+AMS
