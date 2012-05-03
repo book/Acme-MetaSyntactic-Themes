3af7c964b9f49e5e72e849d8f9809da4d769869c
Philippe Bruhat (BooK) <book@cpan.org>
2012-05-03 22:43:21 +0200

    new theme: abba

---
+++ lib/Acme/MetaSyntactic/abba.pm
@@ -0,0 +1,34 @@
+package Acme::MetaSyntactic::abba;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::abba - Singers from the 1970s Swedish pop group.
+
+=head1 DESCRIPTION
+
+ABBA won the 1974 Eurovision Songfestival for Sweden, which started
+their very successful international career that would last into the
+early 1980s.
+
+=head1 CONTRIBUTOR
+
+Submitted Abigail on November 1st, 2005.
+
+Introduced in Acme-MetaSyntactic-Themes version 1.000,
+published on May 7, 2012.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Anni_Frid_Lyngstad Bjorn_Ulvaeus Benny_Andersson Agnetha_Faltskog
