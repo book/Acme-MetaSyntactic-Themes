6962eb9ed11d1b43db5be71153c083ea1a0b468a
Philippe Bruhat (BooK) <book@cpan.org>
2012-05-07 11:09:18 +0200

    browser update for v1.000

--- lib/Acme/MetaSyntactic/browser.pm
+++ lib/Acme/MetaSyntactic/browser.pm
@@ -15,8 +15,10 @@ Acme::MetaSyntactic::browser - The "browser" theme
 Some famous web browsers.
 
 Evolt.org maintains a browser archive at L<http://browsers.evolt.org/>.
-The information in the archive was not used to create this list (but
-may be in the future).
+The information in the archive was not used to create this list.
+
+Wikipedia maintains a large list of web browsers at
+L<https://en.wikipedia.org/wiki/List_of_web_browsers>.
 
 =head1 CONTRIBUTORS
 
@@ -28,7 +30,8 @@ Updated in version 0.70, published on April 17, 2006.
 
 Updated in version 0.96, published on October 16, 2006.
 
-Received its own version number for Acme-MetaSyntactic-Themes version 1.000,
+Updated with Google Chrome, and
+received its own version number for Acme-MetaSyntactic-Themes version 1.000,
 published on May 7, 2012.
 
 =head1 SEE ALSO
@@ -41,3 +44,4 @@ __DATA__
 # names
 mozilla netscape msie mosaic links lynx w3m opera galeon konqueror safari
 camino dillo amaya arachne omniweb planetweb voyager seamonkey iceweasel
+chrome
