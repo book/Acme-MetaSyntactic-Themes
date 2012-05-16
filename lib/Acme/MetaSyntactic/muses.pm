ef903bcfac351be72c60af5c43f8f98f622e59b7
Philippe Bruhat (BooK) <book@cpan.org>
2012-05-16 21:19:22 +0200

    make muses multilingual

--- lib/Acme/MetaSyntactic/muses.pm
+++ lib/Acme/MetaSyntactic/muses.pm
@@ -1,33 +1,56 @@
 package Acme::MetaSyntactic::muses;
 use strict;
-use Acme::MetaSyntactic::List;
-our @ISA = qw( Acme::MetaSyntactic::List );
-our $VERSION = 1.000;
+use Acme::MetaSyntactic::Locale;
+our @ISA = qw( Acme::MetaSyntactic::Locale );
+our $VERSION = 1.001;
 __PACKAGE__->init();
 1;
 
 =head1 NAME
 
-Acme::MetaSyntactic::muses - Greek Muses.
+Acme::MetaSyntactic::muses - Greek Muses
 
 =head1 DESCRIPTION
 
 The nine muses from Greek mythology.
 
-=head1 CONTRIBUTOR
+=head1 CONTRIBUTORS
 
 Submitted by Abigail on October 24, 2005.
 
 Introduced in Acme-MetaSyntactic-Themes version 1.001,
 published on May 14, 2012.
 
+Made multilingual and added translations for I<de>, I<en>, I<eo>, I<es>,
+I<fr>, I<it>, I<la> (the default), I<nl>, I<pl>, I<pt> in version 1.001,
+published in Acme-MetaSyntactic-Themes version 1.002, on May 21, 2012.
+
 =head1 SEE ALSO
 
-L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>,
-L<http://www.eliki.com/portals/fantasy/circle/define.html>.
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
 
 =cut
 
 __DATA__
-# names
-calliope clio erato euterpe melpomene polyhymnia terpsichore thalia urania
+# default
+la
+# names de
+Kalliope Klio Erato Euterpe Melpomene Polyhymnia Terpsichore Thalia  Urania
+# names en
+Calliope Clio Erato Euterpe Melpomene Polyhymnia Terpsichore Thalia  Urania
+# names eo
+Kaliopo  Klio Erato Euterpo Melpomeno Polimnio   Terpsihoro  Talio   Uranio
+# names es
+Caliope  Clio Erato Euterpe Melpomene Polimnia   Terpsicore  Talia   Urania
+# names fr
+Calliope Clio Erato Euterpe Melpomene Polymnie   Terpsichore Thalie  Uranie
+# names it
+Calliope Clio Erato Euterpe Melpomene Polimnia   Tersicore   Talia   Urania
+# names la
+Calliope Clio Erato Euterpe Melpomene Polyhymnia Terpsichore Thalia  Urania
+# names nl
+Kalliope Clio Erato Euterpe Melpomene Polyhymnia Terpsichore Thaleia Urania
+# names pl
+Kalliope Klio Erato Euterpe Melpomene Polihymnia Terpsychora Talia   Urania
+# names pt
+Caliope  Clio Erato Euterpe Melpomene Polimnia   Terpsicore  Talia   Urania
