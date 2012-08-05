908fd5774af60807709e401e2e7914c0845a1d1e
Philippe Bruhat (BooK) <book@cpan.org>
2012-08-06 00:16:22 +0200

    Acme::MetaSyntactic version 1.006 will support comments in the __DATA__ section

--- lib/Acme/MetaSyntactic/soviet.pm
+++ lib/Acme/MetaSyntactic/soviet.pm
@@ -82,37 +82,214 @@ __DATA__
 # default
 radars
 # names radars
-Ball_End Band_Stand Bar_Lock Bee_Hind Big_Fred Big_Net Big_Screen
-Blue_Jay Brass_Tilt Cake_Stand Cross_Bird Cross_Sword Dog_Ear
-Don_Kay Down_Beat Drum_Tilt Egg_Cup Eye_Bowl Fire_Dome Flap_Lid
-Flat_Face Foxfire Front_Door Front_Piece Gun_Dish Half_Cup
-Hawk_Screech Head_Lights Head_Net High_Fix High_Lark High_Sieve
-Hot_Shot Jaybird Kite_Screech Land_Roll Long_Track Muff_Cob
-Owl_Screech Palm_Frond Pat_Hand Peel_Cone Peel_Group Plank_Shave
-Plate_Steer Plinth_Net Pop_Group Pork_Through Puff_Ball Scoop_Pair
-Side_Net Skip_Spin Sky_Watch Slim_Net Small_Fred Small_Yawn Snoop_Pair
-Snoop_Plate Snoop_Slab Snoop_Tray Spin_Trough Spoon_Rest Square_Tie
-Squeeze_Box Straight_Flush Strut_Curve Strut_Pair Sun_Visor
-Tall_Mike Tee_Plinth Thin_Skin Tin_Man Top_Dome Top_Knot Top_Pair
-Top_Plate Top_Sail Top_Steer Top_Trough Trap_Door Two_Spots
+High_Fix       # MiG-21 Su-17
+Jaybird        # MiG-21
+High_Lark      # MiG-23
+Blue_Jay       # MiG-23
+Foxfire        # MiG-25
+Skip_Spin      # Su-15
+Ball_End       # fdc 758
+Don_Kay        # fdc 758
+Spin_Trough    # fdc 758
+Palm_Frond     # fdc 758
+Cross_Bird     # fdc 758
+High_Sieve     # fdc 758
+Head_Net       # fdc 758
+Big_Net        # fdc 758
+Slim_Net       # fdc 758
+Strut_Curve    # fdc 758
+Strut_Pair     # fdc 758
+Top_Trough     # fdc 758
+Top_Pair       # fdc 758
+Top_Sail       # fdc 759
+Top_Steer      # fdc 759
+Top_Plate      # fdc 759
+Plate_Steer    # fdc 759
+Sky_Watch      # fdc 759
+Big_Screen     # fdc 759
+Peel_Group     # fdc 759
+Head_Lights    # fdc 759
+Pop_Group      # fdc 759
+Eye_Bowl       # fdc 759
+Cross_Sword    # fdc 759
+Scoop_Pair     # fdc 759
+Front_Door     # fdc 759
+Trap_Door      # fdc 759
+Top_Dome       # fdc 759
+Top_Knot       # fdc 759
+Cake_Stand     # fdc 759
+Plinth_Net     # fdc 759
+Band_Stand     # fdc 759
+Strut_Pair     # fdc 759
+Square_Tie     # fdc 759
+Plank_Shave    # fdc 759
+Peel_Cone      # fdc 759
+Snoop_Slab     # fdc 759
+Snoop_Pair     # fdc 759
+Snoop_Plate    # fdc 759
+Snoop_Tray     # fdc 759
+Front_Piece    # fdc 759
+Sun_Visor      # fdc 759
+Hawk_Screech   # fdc 759
+Owl_Screech    # fdc 759
+Kite_Screech   # fdc 759
+Muff_Cob       # fdc 759
+Egg_Cup        # fdc 759
+Drum_Tilt      # fdc 759
+Brass_Tilt     # fdc 760
+Squeeze_Box    # fdc 760
+Tee_Plinth     # fdc 760
+Tin_Man        # fdc 760
+Half_Cup       # fdc 760
+Puff_Ball      # fdc 760
+Bee_Hind       # fdc 760
+Down_Beat      # fdc 760
+Tall_Mike      # pl  11
+Big_Fred       # pl  97
+Pork_Through   # pl  97
+Small_Fred     # pl  98
+Small_Yawn     # pl  98
+Hot_Shot       # pl 104
+Dog_Ear        # pl 104
+Gun_Dish       # pl 105
+Land_Roll      # pl 113
+Fire_Dome      # pl 114
+Flap_Lid       # pl 115
+Straight_Flush # pl 120
+Long_Track     # pl 121
+Thin_Skin      # pl 121
+Pat_Hand       # pl 119
+Two_Spots      # pl 122
+Dog_Ear        # pl 122
+Flat_Face      # pl 123
+Spoon_Rest     # pl 123
+Side_Net       # pl 124
+Bar_Lock       # pl 124
 # names aircraft
-Badger Bear Blinder Backfire Bear Beagle Bison Brewer
-Colt Cub Clod Cock Coke Curl Cash Clank Cline Condor
-Crate Coot Classic Candid Camber
-Crusty Careless
-Codling Clobber
-Fagot Fresco Farmer Fishbed Flogger Foxbat Fulcrum Foxhound
-Fitter Fishpot Flagon Fencer Frogfoot Flanker Fiddler
-Firebar Forger
-Hare Hoplite Hound Hook Hip Harke Haze Hind Halo Havoc
-Hormone Hoodlum Helix Hokum
-Mail Mascot May Midget Moss Max Moose Mangrove
+Badger         # Tu-16
+Bear           # Tu-20
+Blinder        # Tu-22
+Backfire       # Tu-22M/-26
+Bear           # Tu-95
+Beagle         # Il-28
+Bison          # M-4
+Brewer         # Yak-28
+Colt           # An-2
+Cub            # An-12
+Clod           # An-14
+Cock           # An-22
+Coke           # An-24
+Curl           # An-26
+Cash           # An-28
+Clank          # An-30
+Cline          # An-32
+Condor         # An-124
+Crate          # Il-14
+Coot           # Il-18/-20
+Classic        # Il-62
+Candid         # Il-76
+Camber         # Il-86
+Crusty         # Tu-134
+Careless       # Tu-154
+Codling        # Yak-40
+Clobber        # Yak-42
+Fagot          # MiG-15
+Fresco         # MiG-17
+Farmer         # MiG-19
+Fishbed        # MiG-21
+Flogger        # MiG-23/-27
+Foxbat         # MiG-25
+Fulcrum        # MiG-29
+Foxhound       # MiG-31
+Fitter         # Su-7/-17
+Fishpot        # Su-9/-11
+Flagon         # Su-15
+Fencer         # Su-24
+Frogfoot       # Su-25
+Flanker        # Su-27
+Fiddler        # Tu-28
+Firebar        # Yak-28P
+Forger         # Yak-38
+Hare           # Mi-1
+Hoplite        # Mi-2
+Hound          # Mi-4
+Hook           # Mi-6
+Hip            # Mi-8/-17
+Harke          # Mi-10
+Haze           # Mi-14
+Hind           # Mi-24
+Halo           # Mi-26
+Havoc          # Mi-28
+Hormone        # Ka-25
+Hoodlum        # Ka-26
+Helix          # Ka-27
+Hokum          # Ka-50
+Mail           # Be-12
+Mascot         # Il-28U
+May            # Il-38
+Midget         # MiG-15UTI
+Moss           # Tu-126
+Max            # Yak-18
+Moose          # Yak-18P
+Mangrove       # Yak-27R
 # names missiles
-Atoll Anab Ash Acrid Apex Aphid Amos Alamo Archer
-Guideline Goa Ganef Gainful Goblet Gecko Gaskin Grail Grumble Gadfly
-Gladiator Gopher Gremlin
-Kennel Kipper Kangaroo Kitchen Kelt Kingfish Kerry Kyle Karen Kedge
-Kent
-Styx Shaddock Sark Serb Starbright Sawfly Siren Sandbox Silex Starfish
-Stallion Snipe Stingray Shipwreck Sturgeon Sampson Sunburn Skiff
-Scorpion Swatter Sagger Spigot Spandrel Spiral Scarab Spider Scud
+Atoll       # AA-2
+Anab        # AA-3
+Ash         # AA-5
+Acrid       # AA-6
+Apex        # AA-7
+Aphid       # AA-8
+Amos        # AA-9
+Alamo       # AA-10
+Archer      # AA-11
+Guideline   # SA-2
+Goa         # SA-3  SA-N-1
+Ganef       # SA-4
+Gainful     # SA-6
+Goblet      #       SA-N-3
+Gecko       # SA-8  SA-N-4
+Gaskin      # SA-9
+Grail       #       SA-N-5
+Grumble     # SA-10 SA-N-6
+Gadfly      # SA-11 SA-N-7
+Gladiator   # SA-12
+Gopher      # SA-13
+Gremlin     # SA-14 SA-N-8
+Kennel      # AS-1
+Kipper      # AS-2
+Kangaroo    # AS-3
+Kitchen     # AS-4
+Kelt        # AS-5
+Kingfish    # AS-6
+Kerry       # AS-7
+Kyle        # AS-9
+Karen       # AS-10
+Kedge       # AS-14
+Kent        # AS-15
+Styx        # SS-N-2
+Shaddock    # SS-N-3
+Sark        # SS-N-5
+Serb        # SS-N-6
+Starbright  # SS-N-7  p 755
+Sawfly      # SS-N-8
+Siren       # SS-N-9
+Sandbox     # SS-N-12
+Silex       # SS-N-14
+Starfish    # SS-N-15 p 757
+Stallion    # SS-N-16
+Snipe       # SS-N-17
+Stingray    # SS-N-18 p 754
+Shipwreck   # SS-N-19
+Sturgeon    # SS-N-20
+Sampson     # SS-N-21
+Sunburn     # SS-N-22
+Skiff       # SS-N-23
+Scorpion    # SS-NX-24
+Swatter     # AT-2
+Sagger      # AT-3
+Spigot      # AT-4
+Spandrel    # AT-5
+Spiral      # AT-6
+Scarab      # SS-21
+Spider      # SS-23
+Scud        # SS1-C
