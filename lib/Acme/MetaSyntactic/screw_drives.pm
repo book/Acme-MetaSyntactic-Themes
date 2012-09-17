bad753002ad3a3b1fcf46fc3e6cc85fcb28f16f1
Philippe Bruhat (BooK) <book@cpan.org>
2012-09-18 01:47:32 +0200

    update list of screw drives from http://en.wikipedia.org/wiki/Screw_drive

--- lib/Acme/MetaSyntactic/screw_drives.pm
+++ lib/Acme/MetaSyntactic/screw_drives.pm
@@ -13,6 +13,8 @@ Acme::MetaSyntactic::screw - The screw drives theme
 
 This theme lists different screw drive types.
 
+See L<https://en.wikipedia.org/wiki/Screw_drive>.
+
 =head1 CONTRIBUTOR
 
 Abigail
@@ -37,23 +39,38 @@ L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
 
 __DATA__
 # names
-slot flat
-Phillips PH
-Pozidriv SupaDriv PZ
-Square
-Robertson
-Hex
-Hex_socket Allen
-Security_hex_socket pin_in_hex_socket
-Torx T TX
-Security_Torx TR
-Tri_Wing
-Torq_set
-Spanner_head Snake_eye
-Triple_square XZN
-Polydrive
-One_way
-Spline_drive
-Double_hex
+square
+hex
+pentagon
+thumbscrew
+slot
+cross cross_recess
+Phillips
+Frearson Reed_and_Prince
+French_recess BNAE_NFL22_070
+JIS_B_1012
+Mortorq
+Pozidriv
+Supadriv
+Robertson Scrulox
+hex_socket Allen
+hexalobular_socket Torx star_drive
+TTAP
+Phillips_square Quadrex Deck_Mate
+breakaway_head
 Bristol
-Pentalobular
+clutch clutch_type_A clutch_type_G
+claw
+double_hex
+line ALR2 ALR3 ALR4 ALR5 ALR6 ALH2 ALH3 ALH4 ALH5 ALH6 ALR3T
+one_way
+pentalobe
+polydrive
+spanner snake_eyes
+spline
+Torq_set
+TA
+TP3
+tri_wing triangular_slotted tri_groove Opsit
+Triple_square XZN
+protruding_obstacle
