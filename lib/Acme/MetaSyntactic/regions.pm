866f599688a3e8208ebf72efdbe51dc3ce5658f2
Philippe Bruhat (BooK) <book@cpan.org>
2012-09-26 01:10:48 +0200

    Divisions of Bangladesh (bd)

--- lib/Acme/MetaSyntactic/regions.pm
+++ lib/Acme/MetaSyntactic/regions.pm
@@ -111,6 +111,9 @@ Zangilan Zaqatala Zardab
 # names bb
 Christ_Church St_Andrew St_George St_James St_John St_Joseph St_Lucy
 St_Michael St_Peter St_Philip St_Thomas
+# names bd
+Barisal Chittagong Chittagong_Hill_Tracts Comilla Dhaka Mymensingh Khulna
+Rajshahi Rangpur Sylhet
 # names de
 Baden_Wurttemberg Bayern Berlin Brandenburg Bremen Hamburg Hessen
 Mecklenburg_Vorpommern Niedersachsen Nordrhein_Westfalen Rheinland_Pfalz
