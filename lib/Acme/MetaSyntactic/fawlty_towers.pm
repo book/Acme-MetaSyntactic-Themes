1c8be96d0757242d36d2ac77a45a81b483ef4381
Philippe Bruhat (BooK) <book@cpan.org>
2012-09-30 22:34:05 +0200

    merge both fawlty_towers themes into a single one

--- lib/Acme/MetaSyntactic/fawlty_towers.pm
+++ lib/Acme/MetaSyntactic/fawlty_towers.pm
@@ -1,7 +1,7 @@
 package Acme::MetaSyntactic::fawlty_towers;
 use strict;
-use Acme::MetaSyntactic::List;
-our @ISA = qw( Acme::MetaSyntactic::List );
+use Acme::MetaSyntactic::MultiList;
+our @ISA = qw( Acme::MetaSyntactic::MultiList );
 __PACKAGE__->init();
 1;
 
@@ -11,10 +11,12 @@ Acme::MetaSyntactic::fawlty_towers - Characters from Fawlty Towers.
 
 =head1 DESCRIPTION
 
-The main characters from the British sitcom I<Fawlty Towers>, of 
-which 12 episodes where made in the 1975 and 1979.
+The main characters and episodes from the British sitcom I<Fawlty Towers>,
+of which 12 episodes where made in the 1975 and 1979.
 
-Cast:
+See L<http://www.fawltysite.net/>.
+
+=head2 Cast
 
  ACTOR                    ROLE
  John Cleese              Basil Fawlty
@@ -26,17 +28,33 @@ Cast:
  Renee Roberts            Miss Gatsby
  Brian Hall               Terry the Chef
 
+=head2 Episodes
+
+ SERIES 1 (1975)          SERIES 2 (1979)
+ A Touch of Class         Communication Problems
+ The Builders             The Psychiatrist
+ The Wedding Party        Waldorf Salad
+ The Hotel Inspectors     The Kipper and the Corpse
+ Gourmet Night            The Anniversary
+ The Germans              Brasil the Rat
+
 =head1 CONTRIBUTOR
 
-Original contributor: Abigail
+Abigail
 
 =head1 SEE ALSO
 
-L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>,
-L<http://www.fawltysite.net/>.
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::MultiList>.
 
 =cut
 
 __DATA__
-# names
+# default
+characters
+# names characters
 Basil Sybil Polly Manual Gowen Tibbs Gatsby Terry
+# names episodes
+A_Touch_of_Class             The_Builders        The_Wedding_Party
+The_Hotel_Inspectors         Gourmet_Night       The_Germans
+Communication_Problems       The_Psychiatrist    Waldorf_Salad
+The_Kipper_and_the_Corpse    The_Anniversary     Brasil_the_Rat
