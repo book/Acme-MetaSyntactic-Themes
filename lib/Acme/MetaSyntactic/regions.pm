3c029603779956c386c545a740d93a4861164b48
Philippe Bruhat (BooK) <book@cpan.org>
2012-10-02 00:21:42 +0200

    disticts of Cyprus (cy)

--- lib/Acme/MetaSyntactic/regions.pm
+++ lib/Acme/MetaSyntactic/regions.pm
@@ -221,6 +221,8 @@ Ribeira_Grande Ribeira_Grande_de_Santiago Sal Santa_Catarina
 Santa_Catarina_do_Fogo Santa_Cruz Sao_Domingos Sao_Filipe
 Sao_Lourenco_dos_Orgaos Sao_Miguel Sao_Salvador_do_Mundo Sao_Vicente
 Tarrafal Tarrafal_de_Sao_Nicolau
+# names cy
+Ammochostos Keryneia Larnaka Lefkosia Lemesos Pafos
 # names dk
 Hovedstaden Midtjylland Nordjylland Sjaelland Syddanmark
 # names fr
