ec6c33386ea9a66af939c2623f45e416d4b1f18f
Philippe Bruhat (BooK) <book@cpan.org>
2012-10-02 21:46:41 +0200

    ISO_3166-2:AT

--- lib/Acme/MetaSyntactic/regions.pm
+++ lib/Acme/MetaSyntactic/regions.pm
@@ -104,3 +104,7 @@ Cordoba Corrientes Entre_Rios Formosa Jujuy La_Pampa La_Rioja Mendoza
 Misiones Neuquen Rio_Negro Salta San_Juan San_Luis Santa_Cruz Santa_Fe
 Santiago_del_Estero Tierra_del_Fuego Tucuman
 
+# names at
+Burgenland Karnten Niederosterreich Oberosterreich Salzburg Steiermark
+Tirol Vorarlberg Wien
+
