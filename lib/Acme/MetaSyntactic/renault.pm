9f8e2443a86030de826622481dcc657f3bab9860
Philippe Bruhat (BooK) <book@cpan.org>
2012-10-24 00:06:27 +0200

    refresh all car types using wikipedia

--- lib/Acme/MetaSyntactic/renault.pm
+++ lib/Acme/MetaSyntactic/renault.pm
@@ -1,7 +1,7 @@
 package Acme::MetaSyntactic::renault;
 use strict;
-use Acme::MetaSyntactic::List;
-our @ISA = qw( Acme::MetaSyntactic::List );
+use Acme::MetaSyntactic::MultiList;
+our @ISA = qw( Acme::MetaSyntactic::MultiList );
 __PACKAGE__->init();
 1;
 
@@ -15,36 +15,159 @@ Car types from the French car manufacturer I<Renault>.
 
 =head1 CONTRIBUTOR
 
-Original contributor: Abigail
+Abigail
+
+=head1 CHANGES
+
+=over 4
+
+=item *
+
+2012-10-24
+
+Made into a multilist, and updated with the models listed on the French
+Wikipedia page L<https://fr.wikipedia.org/wiki/V%C3%A9hicules_Renault>.
+
+=item *
+
+2005-10-27
+
+Submitted by Abigail.
+
+=back
 
 =head1 SEE ALSO
 
-L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>,
-L<http://www.renault.com>.
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::MultiList>.
 
 =cut
 
 __DATA__
 # names
-Renault_Ax Renault_12CV Renault_20CV Type_A Type_AG Type_AG_1 Type_AH
-Type_AM Type_AI Type_CF Type_DQ Type_ET Type_B Type_C Type_D Type_E
-Type_G Type_H Type_J Type_L Type_M Type_Na Type_Nb Type_S Type_Nc Type_Q
-Type_Ua Type_Ue Type_R Type_T Type_Ub Type_Uc Type_Ud Type_V Type_AS
-Type_X Type_X_1 Type_Y
-Celtaquatre Juvaquatre Nervastella Primaquatre Reinastella
-Viva_Grand_Sport Vivaquatre Vivasix
-Renault_4CV Caravelle Colorale Dauphine Dauphinoise Floride Frégate Ondine
-Renault_3 Renault_4 Renault_5 Renault_6 Renault_7 Renault_8 Renault_9
-Renault_10 Renault_11 Renault_12 Renault_14 Renault_15 Renault_16
-Renault_17 Renault_18 Renault_19 Renault_20 Renault_21 Renault_25
+Renault_40CV
+Renault_KJ
+Renault_MT
+Renault_KZ
+Renault_NN
+Renault_Monasix
+Renault_Vivasix
+Renault_Monastella
+Renault_Vivastella
+Renault_Reinastella
+Renault_Nervastella
+Renault_Monaquatre
+Renault_Primaquatre
+Renault_Vivaquatre
+Renault_Primastella
+Renault_Celtaquatre
+Renault_Viva_Grand_Sport
+Renault_Reinasport
+Renault_Nerva_Grand_Sport
+Renault_Juvaquatre
+Renault_Novaquatre
+Renault_Suprastella
+Renault_4CV
+Renault_Colorale
+Renault_Fregate
+Renault_Dauphine
+Renault_Dauphinoise
+Renault_Caravelle
+Renault_Floride
+Renault_Estafette
+Renault_4
+Renault_4_Fourgonnette
+Renault_8_et_10
+Renault_Rambler
+Renault_16
+Renault_6
+Renault_12
+Renault_15
+Renault_17
+Renault_5
 Renault_30
-Alliance Encore Fuego Medallion Premier Torino
-Avantime Clio Espace Kangoo Laguna Logan Mégane Modus Safrane Scénic
-Twingo Vel_Satis Spider
-A106 A108 A110 A305 A310 A610
-Argos Be_Bop BRV Espace_F1 Ellypse EPURE EVE Etoile_Filante Fiftie Fluence
-Initiale Koléos Ludo Next Pangea Racoon Talisman Velsatis VESTA_2 Wind
-ZO Zoé
-Estafette Fourgonette Kangoo_Express Kerax Magnum Mascott Master Midlum
-Trafic
-CityBus PR_100
+Renault_14
+Renault_20
+Renault_18
+Renault_Fuego
+Renault_Master
+Renault_9
+Renault_11
+Renault_Trafic
+Renault_Supercinq
+Renault_25
+Renault_Espace
+Renault_Express
+Renault_21
+Renault_19
+Renault_Clio
+Renault_Espace_II
+Renault_Safrane
+Renault_Twingo
+Renault_Laguna
+Renault_Megane
+Renault_Scenic
+Renault_Espace_III
+Renault_Spider
+Renault_Kangoo
+Renault_Master
+Renault_Clio_II
+Renault_Laguna_II
+Renault_Espace_IV
+Renault_Avantime
+Renault_Trafic_II
+Renault_Megane_II
+Renault_Vel_Satis
+Renault_Scenic_II
+Renault_Modus
+Renault_Clio_III
+Renault_Twingo_II
+Renault_Laguna_III
+Renault_Megane_III
+Renault_Koleos
+Renault_Kangoo_II
+Renault_Fluence
+Renault_Scenic_III
+Renault_Latitude
+Renault_Wind
+Renault_Master
+Renault_Twizy
+Renault_Clio_IV
+Renault_Zoe
+# names alpine
+Alpine_A106
+Alpine_A108
+Alpine_A110
+Alpine_A310
+Alpine_GTA
+Alpine_A610
+# names usa
+Renault_Le_Car
+Renault_18i_Sportwagon
+Renault_Alliance
+Renault_Encore
+Renault_Fuego_GTA
+Renault_Alpine_GTA
+Renault_Medallion
+Renault_Premier
+# names emerging_countries
+Renault_Logan
+Renault_Sandero
+Renault_Symbol_II
+Renault_Duster
+Renault_Pulse
+# names formula_1
+Renault_RS01
+Renault_RS10
+Renault_RE20
+Renault_RE30
+Renault_RE40
+Renault_RE50
+Renault_RE60
+Renault_R202
+Renault_R23
+Renault_R24
+Renault_R25
+Renault_R26
+Renault_R27
+Renault_R28
+Renault_R29
