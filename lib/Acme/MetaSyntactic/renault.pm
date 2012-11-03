f28209c1fd744fc62b537c0e9ddfd72411203084
Philippe Bruhat (BooK) <book@cpan.org>
2012-11-03 08:54:21 +0100

    prepare renault for release

--- lib/Acme/MetaSyntactic/renault.pm
+++ lib/Acme/MetaSyntactic/renault.pm
@@ -2,6 +2,7 @@ package Acme::MetaSyntactic::renault;
 use strict;
 use Acme::MetaSyntactic::MultiList;
 our @ISA = qw( Acme::MetaSyntactic::MultiList );
+our $VERSION = '1.000';
 __PACKAGE__->init();
 1;
 
@@ -20,6 +21,12 @@ Abigail
 =head1 CHANGES
 
 =over 4
+
+=item *
+
+2012-11-05 - v1.000
+
+Published in Acme-MetaSyntactic-Themes version 1.026.
 
 =item *
 
