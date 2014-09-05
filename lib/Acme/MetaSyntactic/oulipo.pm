917fc47eb830b19abd9e1267cfde76376d145e80
Philippe Bruhat (BooK) <book@cpan.org>
2014-09-06 01:29:30 +0200

    add the dates when members joined Oulipo

--- lib/Acme/MetaSyntactic/oulipo.pm
+++ lib/Acme/MetaSyntactic/oulipo.pm
@@ -34,155 +34,155 @@ __PACKAGE__->init(
 
 =item *
 
-Noël ARNAUD (1960-2003)
+Noël ARNAUD (1919-2003), founding member.
 
 =item *
 
-Michèle AUDIN (2009-)
+Michèle AUDIN (1954-), joined in 2009.
 
 =item *
 
-Valérie BEAUDOUIN (2003-)
+Valérie BEAUDOUIN (1968-), joined in 2003.
 
 =item *
 
-Marcel BÉNABOU (1969-)
+Marcel BÉNABOU (1939-), joined in 1970.
 
 =item *
 
-Jacques BENS (1960-2001)
+Jacques BENS (1931-2001), founding member.
 
 =item *
 
-Claude BERGE (1960-2002)
+Claude BERGE (1926-2002), founding member.
 
 =item *
 
-André BLAVIER (1961-2001)
+André BLAVIER (1922-2001), foreign correspondent.
 
 =item *
 
-Paul BRAFFORT (1961-)
+Paul BRAFFORT (1923-), joined in 1961.
 
 =item *
 
-Italo CALVINO (1974-1985)
+Italo CALVINO (1923-1985), joined in 1974.
 
 =item *
 
-François CARADEC (1983-2008)
+François CARADEC (1924-2008), joined in 1983.
 
 =item *
 
-Bernard CERQUIGLINI (1995-)
+Bernard CERQUIGLINI (1947-), joined in 1995.
 
 =item *
 
-Ross CHAMBERS (1961-)
+Ross CHAMBERS (1932-), joined in 1961.
 
 =item *
 
-Stanley CHAPMAN (1961-2009)
+Stanley CHAPMAN (1925-2009), joined in 1961.
 
 =item *
 
-Marcel DUCHAMP (1961-1968)
+Marcel DUCHAMP (1887-1968), joined in 1962.
 
 =item *
 
-Jacques DUCHATEAU (1929-)
+Jacques DUCHATEAU (1929-), founding member.
 
 =item *
 
-Luc ETIENNE (1970-1984)
+Luc ETIENNE (1908-1984), joined in 1970.
 
 =item *
 
-Frédéric FORTE (2005-)
+Frédéric FORTE (1973-), joined in 2005.
 
 =item *
 
-Paul FOURNEL (1972-)
+Paul FOURNEL (1947-), joined in 1972.
 
 =item *
 
-Anne F. GARRÉTA (2000-)
+Anne F. GARRÉTA (1962-), joined in 2000.
 
 =item *
 
-Michelle GRANGAUD (1995-)
+Michelle GRANGAUD (1941-), joined in 1995.
 
 =item *
 
-Jacques JOUET (1983-)
+Jacques JOUET (1947-), joined in 1983.
 
 =item *
 
-LATIS (1960-1973)
+LATIS (1913-1973), founding member.
 
 =item *
 
-François LE LIONNAIS (1960-1984)
+François LE LIONNAIS (1901-1984), founder.
 
 =item *
 
-Hervé LE TELLIER (1992-)
+Hervé LE TELLIER (1957-), joined in 1992.
 
 =item *
 
-Etienne LÉCROART (2012-)
+Étienne LÉCROART (1960-), joined in 2012.
 
 =item *
 
-Jean LESCURE (1960-2005)
+Jean LESCURE (1912-2005), founding member.
 
 =item *
 
-Daniel LEVIN BECKER (2009-)
+Daniel LEVIN BECKER (1984-), joined in 2009.
 
 =item *
 
-Harry MATHEWS (1973-)
+Harry MATHEWS (1930-), joined in 1973.
 
 =item *
 
-Michèle MÉTAIL (1975-)
+Michèle MÉTAIL (1950-), joined in 1975.
 
 =item *
 
-Ian MONK (1998-)
+Ian MONK (1960-), joined in 1998.
 
 =item *
 
-Oskar PASTIOR (1992-2006)
+Oskar PASTIOR (1927-2006), joined in 1992.
 
 =item *
 
-Georges PEREC (1967-1982)
+Georges PEREC (1936-1982), joined in 1967.
 
 =item *
 
-Raymond QUENEAU (1960-1976)
+Raymond QUENEAU (1903-1976), founder.
 
 =item *
 
-Jean QUEVAL (1960-1990)
+Jean QUEVAL (1913-1990), founding member.
 
 =item *
 
-Pierre ROSENSTIEHL (1992-)
+Pierre ROSENSTIEHL (1933-), joined in 1992.
 
 =item *
 
-Jacques ROUBAUD (1966-)
+Jacques ROUBAUD (1932-), joined in 1966.
 
 =item *
 
-Olivier SALON (2000-)
+Olivier SALON (1955-), joined in 2000.
 
 =item *
 
-Albert-Marie SCHMIDT (1960-1966)
+Albert-Marie SCHMIDT (1901-1966), founding member.
 
 =back
 
