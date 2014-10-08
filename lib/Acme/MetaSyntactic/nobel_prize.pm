fd029cd51e6c7588e910aea889d5446c7ead484d
Philippe Bruhat (BooK) <book@cpan.org>
2014-10-08 11:45:00 +0200

    2014 Nobel Prize in Chemistry: Eric Betzig, Stefan W. Hell and William E. Moerner

--- lib/Acme/MetaSyntactic/nobel_prize.pm
+++ lib/Acme/MetaSyntactic/nobel_prize.pm
@@ -1017,6 +1017,9 @@ The winners are:
  2013   Physics     François Englert
  2013   Physics     Peter Higgs
 
+ 2014   Chemistry   Eric Betzig
+ 2014   Chemistry   Stefan W. Hell
+ 2014   Chemistry   William E. Moerner
  2014   Medecine    John O'Keefe
  2014   Medecine    May-Britt Moser
  2014   Medecine    Edvard I. Moser
