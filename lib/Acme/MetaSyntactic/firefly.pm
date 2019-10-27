8177e52750d6cba267ee3d35dfdce8cb9695d0a1
Sergey Romanov <sromanov@cpan.org>
2019-10-28 02:02:14 +0300

    new theme: firefly

---
+++ lib/Acme/MetaSyntactic/firefly.pm
@@ -0,0 +1,50 @@
+package Acme::MetaSyntactic::firefly;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+our $VERSION = '1.000';
+__PACKAGE__->init();
+1;
+=pod
+
+=head1 NAME
+
+Acme::MetaSyntactic::firefly - The Firefly theme
+
+=head1 DESCRIPTION
+
+The characters from that space Western TV show.
+
+=head1 CONTRIBUTOR
+
+Sergey Romanov
+
+=head1 CHANGES
+
+=over 4
+
+=item *
+
+2019-10-28 - v1.000
+
+Submitted by sromanov
+
+=back
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Mal
+Zoe
+Wash
+Kaylee
+River
+Simon
+Inara
+Book
+Jayne
